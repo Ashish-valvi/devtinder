dbb9fb7a51761ba965c18424d8b1605bd6cc6e0b
Ashish-valvi <ashishvalvi.nvp@gmail.com>
2025-01-04 22:36:08 +0530

    server created

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 7000;
+
+app.use("/test",(req,res)=>{
+   res.send("this is a response from /test ")
+})
+app.use("/hello",(req,res)=>{
+    res.send("this is a response from /hello ")
+ })
+
+app.listen(port,()=>{
+    console.log(`server is rumming successfully on ${port}`)
+});
